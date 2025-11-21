46ab1e148105f6ff1cc0d3e896d942714659d42e
daylilystudio <ddaylily@gmail.com>
2025-11-21 16:50:30 +0800

    optimize not found

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -1,31 +1,24 @@
 import './globals.css';
 
-import Link from 'next/link';
-import { getLocale, getTranslations } from 'next-intl/server';
+import { SITE_URL } from '@/utils/const';
 
-import { defaultLocale } from '@/i18n/routing';
-
-// 這個頁面無法被翻譯，因為它在 locale 路由之外，所以用 getLocale 取得目前語言
-export default async function RootNotFound() {
-  const locale = await getLocale();
-  const t = await getTranslations({ locale, namespace: 'common' });
-
+// 此頁放在 [locale] 裡會無法找到自訂 404 頁面
+// 無法使用語系，因為此頁在 locale 路由之外
+export default function RootNotFound() {
   return (
-    <html lang={locale}>
+    <html>
       <body>
         <div className="font-sans flex flex-col items-center justify-center min-h-screen gap-5">
-          <h1 style={{ fontSize: '2rem', fontWeight: 600 }}>
-            {t('404.title')}
-          </h1>
-          <p style={{ fontSize: '1.125rem' }}>{t('404.description')}</p>
+          <h1 style={{ fontSize: '2rem', fontWeight: 600 }}>404</h1>
+          <p style={{ fontSize: '1.125rem' }}>找不到頁面</p>
           <div>
-            {/* 提供明確的語言連結 */}
-            <Link
+            {/* 用 next/link 會停滯在此頁，故改用 <a> */}
+            <a
               className="text-gray-400 hover:text-blue-500 transition-colors"
-              href={locale !== defaultLocale ? `/${locale}` : `/`}
+              href={SITE_URL}
             >
-              {t('backToHome')}
-            </Link>
+              Back to Home
+            </a>
           </div>
         </div>
       </body>
