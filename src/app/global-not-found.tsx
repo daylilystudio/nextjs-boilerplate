c2d66872bde91ff72ba3b59e9095422de6be136b
daylilystudio <ddaylily@gmail.com>
2025-12-01 15:07:23 +0800

    404 metadata

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -1,10 +1,17 @@
 import './globals.css';
 
+import { Metadata } from 'next';
+
 import { SITE_URL, zenMaruGothic } from '@/utils/const';
 
 // 處理 「連語系都無法辨識」 或 「根路徑錯誤」 的情況
 // This page renders when a route like `/unknown.txt` is requested.
 // In this case, the layout at `app/[locale]/layout.tsx` receives an invalid value as the `[locale]` param and calls `notFound()`.
+
+export const metadata: Metadata = {
+  title: '404 - Page Not Found',
+  description: 'The page you are looking for does not exist.',
+};
 
 export default function GlobalNotFound() {
   return (
