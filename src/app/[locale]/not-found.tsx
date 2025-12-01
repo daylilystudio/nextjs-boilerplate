1e49c8863c5c29c2a9cbf754d6df73d2b60d91c8
daylilystudio <ddaylily@gmail.com>
2025-12-01 14:08:14 +0800

    edit not-found

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,7 +1,11 @@
 import { setRequestLocale } from 'next-intl/server';
 
 import NotFound from '@/components/NotFound';
-import type { Locale } from '@/i18n/routing';
+import { type Locale, locales } from '@/i18n/routing';
+
+export async function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
 
 // Note that `app/[locale]/[...rest]/page.tsx` is necessary for this page to render.
 export default async function NotFoundPage({
